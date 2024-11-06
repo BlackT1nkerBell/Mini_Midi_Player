c08ecd99aad06572a78af2ad0cc6676e9b6484a6
BlackT1nkerBell <paigevbell@gmail.com>
2024-11-05 23:57:16 -0500

    Completed music for A7 and Tweaked A6's
    "When Your Friends Ask You to Play The Piano."
    
    Basically starts with mozart and ends in a joke.

--- sketch_nov1a.ino
+++ sketch_nov1a.ino
@@ -583,7 +583,7 @@ if(valueA6 <touch){
   CircuitPlayground.playTone(659,333/2,false);
   delay(333/2);
   CircuitPlayground.playTone(659,333/2,false);
-  delay(10);
+  delay(333/2);
   CircuitPlayground.playTone(523,333/2,false);
   delay(10);
   CircuitPlayground.playTone(659,333,false);
@@ -594,7 +594,7 @@ if(valueA6 <touch){
   CircuitPlayground.playTone(659,333/2,false);
   delay(333/2);
   CircuitPlayground.playTone(659,333/2,false);
-  delay(10);
+  delay(333/2);
   CircuitPlayground.playTone(523,333/2,false);
   delay(10);
   CircuitPlayground.playTone(659,333,false);
@@ -604,6 +604,22 @@ if(valueA6 <touch){
   delay(333);
   CircuitPlayground.playTone(392,333,false);
   delay(333);
+
+  CircuitPlayground.playTone(165,333,false);
+  delay(333/2);
+  CircuitPlayground.playTone(196,333,false);
+  delay(10);
+  CircuitPlayground.playTone(165,333,false);
+  delay(333/2);
+
+  CircuitPlayground.playTone(220,333,false);
+  delay(10);
+  CircuitPlayground.playTone(247,333,false);
+  delay(10);
+  CircuitPlayground.playTone(466,333/2,false);
+  delay(10);
+  CircuitPlayground.playTone(220,333,false);
+  delay(10);
 
 } else{
 CircuitPlayground.setPixelColor(0,0,0,0);
@@ -619,30 +635,63 @@ CircuitPlayground.setPixelColor(0,0,0,0);
 }
 
 
-// //A7
-// valueA7 = analogRead(A7);
-// if(valueA7 <touch){
-//  CircuitPlayground.setPixelColor(0,0xfcba03);
-//  CircuitPlayground.setPixelColor(1,0xfcba03);
-//   CircuitPlayground.setPixelColor(2,0xfcba03);
-//  CircuitPlayground.setPixelColor(3,0xfcba03);
-//   CircuitPlayground.setPixelColor(4,0xfcba03);
-//  CircuitPlayground.setPixelColor(5,0xfcba03);
-//   CircuitPlayground.setPixelColor(6,0xfcba03);
-//  CircuitPlayground.setPixelColor(7,0xfcba03);
-//   CircuitPlayground.setPixelColor(8,0xfcba03);
-//  CircuitPlayground.setPixelColor(9,0xfcba03);
-//   CircuitPlayground.playTone(50,1000);
-// } else{
-// CircuitPlayground.setPixelColor(0,0,0,0);
-//  CircuitPlayground.setPixelColor(1,0,0,0);
-//   CircuitPlayground.setPixelColor(2,0,0,0);
-//  CircuitPlayground.setPixelColor(3,0,0,0);
-//   CircuitPlayground.setPixelColor(4,0,0,0);
-//  CircuitPlayground.setPixelColor(5,0,0,0);
-//   CircuitPlayground.setPixelColor(6,0,0,0);
-//  CircuitPlayground.setPixelColor(7,0,0,0);
-//   CircuitPlayground.setPixelColor(8,0,0,0);
-//  CircuitPlayground.setPixelColor(9,0,0,0);
-// }
+//A7
+valueA7 = analogRead(A7);
+if(valueA7 <touch){
+ CircuitPlayground.setPixelColor(0,0xfcba03);
+ CircuitPlayground.setPixelColor(1,0xfcba03);
+  CircuitPlayground.setPixelColor(2,0xfcba03);
+ CircuitPlayground.setPixelColor(3,0xfcba03);
+  CircuitPlayground.setPixelColor(4,0xfcba03);
+ CircuitPlayground.setPixelColor(5,0xfcba03);
+  CircuitPlayground.setPixelColor(6,0xfcba03);
+ CircuitPlayground.setPixelColor(7,0xfcba03);
+  CircuitPlayground.setPixelColor(8,0xfcba03);
+ CircuitPlayground.setPixelColor(9,0xfcba03);
+
+  CircuitPlayground.playTone(659,200,false);
+  delay(10);
+  CircuitPlayground.playTone(622,200,false);
+  delay(10);
+  CircuitPlayground.playTone(659,200,false);
+  delay(10);
+  CircuitPlayground.playTone(622,200,false);
+  delay(10);
+  CircuitPlayground.playTone(659,200,false);
+  delay(10);
+  CircuitPlayground.playTone(330,200,false);
+  delay(10);
+  CircuitPlayground.playTone(587,200,false);
+  delay(10);
+  CircuitPlayground.playTone(523,200,false);
+  delay(10);
+
+  CircuitPlayground.playTone(440,250,false);
+  delay(10);
+  CircuitPlayground.playTone(330,125,false);
+  delay(10);
+  CircuitPlayground.playTone(330,125,false);
+  delay(10);
+  CircuitPlayground.playTone(349,250,false);
+  delay(10);
+  CircuitPlayground.playTone(330,250,false);
+  delay(250);
+  CircuitPlayground.playTone(415,250,false);
+  delay(10);
+  CircuitPlayground.playTone(440,250,false);
+  delay(250);
+  CircuitPlayground.playTone(220,250,false);
+
+} else{
+CircuitPlayground.setPixelColor(0,0,0,0);
+ CircuitPlayground.setPixelColor(1,0,0,0);
+  CircuitPlayground.setPixelColor(2,0,0,0);
+ CircuitPlayground.setPixelColor(3,0,0,0);
+  CircuitPlayground.setPixelColor(4,0,0,0);
+ CircuitPlayground.setPixelColor(5,0,0,0);
+  CircuitPlayground.setPixelColor(6,0,0,0);
+ CircuitPlayground.setPixelColor(7,0,0,0);
+  CircuitPlayground.setPixelColor(8,0,0,0);
+ CircuitPlayground.setPixelColor(9,0,0,0);
+}
 }
