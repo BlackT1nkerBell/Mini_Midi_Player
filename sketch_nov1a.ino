533619ac0faa528f3daaebaaad338654003423e9
BlackT1nkerBell <paigevbell@gmail.com>
2024-11-07 17:45:29 -0500

    Speed dial and color change
    Created variables that held an initial speed (iSpeed), decreasing number, and increasing number.
    
    Speed contorlled by buttons, color controlled by switch.

--- sketch_nov1a.ino
+++ sketch_nov1a.ino
@@ -1,5 +1,6 @@
 #include <Adafruit_CircuitPlayground.h>
 
+//Holders for capacitive A1 through A7
 int valueA1;
 int valueA2;
 int valueA3;
@@ -8,7 +9,14 @@ int valueA5;
 int valueA6;
 int valueA7;
 int c;
+//Holds 
 int touch;
+int iSpeed;
+int speedUp;
+int speedDown;
+int left;
+int right;
+int sw;
 ///////////////////////////////////////////////////////////////////////////////
 void setup() {
 CircuitPlayground.begin();
@@ -16,72 +24,79 @@ CircuitPlayground.begin();
 
 void loop() {
 touch = 6;
-//ColorSwitch
-//if (CircuitPlayground.slideSwitch()){
-// c = 0xfcba03;
-//} else {
-//c = 0x033dfc;
-//}
+iSpeed = 1;
+speedUp = 1.5;
+speedDown = 0.50;
+left = CircuitPlayground.leftButton();
+right = CircuitPlayground.rightButton();
+sw = CircuitPlayground.slideSwitch();
 
 
-CircuitPlayground.setBrightness(150);
-//Initial location of "ball"; the "ground"
-//CircuitPlayground.setPixelColor(6,c);
+CircuitPlayground.setBrightness(50);
+
+if (sw){
+c = 0xfcba03;
+iSpeed = iSpeed + speedUp;
+} else {
+c = 0x6df5f7;
+iSpeed = iSpeed - speedDown;
+}
+
 //A1
 valueA1 = analogRead(A1);
 if(valueA1 <touch){
 
- CircuitPlayground.setPixelColor(0,0xfcba03);
- CircuitPlayground.setPixelColor(1,0xfcba03);
-  CircuitPlayground.setPixelColor(2,0xfcba03);
- CircuitPlayground.setPixelColor(3,0xfcba03);
-  CircuitPlayground.setPixelColor(4,0xfcba03);
- CircuitPlayground.setPixelColor(5,0xfcba03);
-  CircuitPlayground.setPixelColor(6,0xfcba03);
- CircuitPlayground.setPixelColor(7,0xfcba03);
-  CircuitPlayground.setPixelColor(8,0xfcba03);
- CircuitPlayground.setPixelColor(9,0xfcba03);
+ CircuitPlayground.setPixelColor(0,c);
+ CircuitPlayground.setPixelColor(1,c);
+  CircuitPlayground.setPixelColor(2,c);
+ CircuitPlayground.setPixelColor(3,c);
+  CircuitPlayground.setPixelColor(4,c);
+ CircuitPlayground.setPixelColor(5,c);
+  CircuitPlayground.setPixelColor(6,c);
+ CircuitPlayground.setPixelColor(7,c);
+  CircuitPlayground.setPixelColor(8,c);
+ CircuitPlayground.setPixelColor(9,c);
 
 //Megalovania
-  CircuitPlayground.playTone(147,500/3,false);
+  CircuitPlayground.playTone(147,500/3*iSpeed,false);
   delay(10);
-  CircuitPlayground.playTone(147,500/3,false);
+  CircuitPlayground.playTone(147,500/3*iSpeed,false);
   delay(10);
-  CircuitPlayground.playTone(294,500/3,false);
+  CircuitPlayground.playTone(294,500/3*iSpeed,false);
   delay(10);
-  CircuitPlayground.playTone(220,250,false);
+  CircuitPlayground.playTone(220,250*iSpeed,false);
   delay(500/5);
-  CircuitPlayground.playTone(208,500/5,false);
+  CircuitPlayground.playTone(208,500/5*iSpeed,false);
   delay(500/5);
-  CircuitPlayground.playTone(196,500/5,false);
+  CircuitPlayground.playTone(196,500/5*iSpeed,false);
   delay(500/5);
-  CircuitPlayground.playTone(175,250,false);
+  CircuitPlayground.playTone(175,250*iSpeed,false);
   delay(10);
-  CircuitPlayground.playTone(147,125,false);
+  CircuitPlayground.playTone(147,125*iSpeed,false);
   delay(10);
-  CircuitPlayground.playTone(175,125,false);
+  CircuitPlayground.playTone(175,125*iSpeed,false);
   delay(10);
-  CircuitPlayground.playTone(196,125,false);
+  CircuitPlayground.playTone(196,125*iSpeed,false);
 
-  CircuitPlayground.playTone(147,500/3,false);
+  CircuitPlayground.playTone(147,500/3*iSpeed,false);
   delay(10);
-  CircuitPlayground.playTone(147,500/3,false);
+  CircuitPlayground.playTone(147,500/3*iSpeed,false);
   delay(10);
-  CircuitPlayground.playTone(294,500/3,false);
+  CircuitPlayground.playTone(294,500/3*iSpeed,false);
   delay(10);
-  CircuitPlayground.playTone(220,250,false);
+  CircuitPlayground.playTone(220,250*iSpeed,false);
   delay(500/5);
-  CircuitPlayground.playTone(208,500/5,false);
+  CircuitPlayground.playTone(208,500/5*iSpeed,false);
   delay(500/5);
-  CircuitPlayground.playTone(196,500/5,false);
+  CircuitPlayground.playTone(196,500/5*iSpeed,false);
   delay(500/5);
-  CircuitPlayground.playTone(175,250,false);
+  CircuitPlayground.playTone(175,250*iSpeed,false);
   delay(10);
-  CircuitPlayground.playTone(147,125,false);
+  CircuitPlayground.playTone(147,125*iSpeed,false);
   delay(10);
-  CircuitPlayground.playTone(175,125,false);
+  CircuitPlayground.playTone(175,125*iSpeed,false);
   delay(10);
-  CircuitPlayground.playTone(196,125,false);
+  CircuitPlayground.playTone(196,125*iSpeed,false);
 } else{
 CircuitPlayground.setPixelColor(0,0,0,0);
  CircuitPlayground.setPixelColor(1,0,0,0);
@@ -98,16 +113,16 @@ CircuitPlayground.setPixelColor(0,0,0,0);
 //A2
 valueA2 = analogRead(A2);
 if(valueA2 <touch){
- CircuitPlayground.setPixelColor(0,0xfcba03);
- CircuitPlayground.setPixelColor(1,0xfcba03);
-  CircuitPlayground.setPixelColor(2,0xfcba03);
- CircuitPlayground.setPixelColor(3,0xfcba03);
-  CircuitPlayground.setPixelColor(4,0xfcba03);
- CircuitPlayground.setPixelColor(5,0xfcba03);
-  CircuitPlayground.setPixelColor(6,0xfcba03);
- CircuitPlayground.setPixelColor(7,0xfcba03);
-  CircuitPlayground.setPixelColor(8,0xfcba03);
- CircuitPlayground.setPixelColor(9,0xfcba03);
+ CircuitPlayground.setPixelColor(0,c);
+ CircuitPlayground.setPixelColor(1,c);
+  CircuitPlayground.setPixelColor(2,c);
+ CircuitPlayground.setPixelColor(3,c);
+  CircuitPlayground.setPixelColor(4,c);
+ CircuitPlayground.setPixelColor(5,c);
+  CircuitPlayground.setPixelColor(6,c);
+ CircuitPlayground.setPixelColor(7,c);
+  CircuitPlayground.setPixelColor(8,c);
+ CircuitPlayground.setPixelColor(9,c);
 
 CircuitPlayground.playTone(196,500,false);
   delay(10);
@@ -184,16 +199,16 @@ CircuitPlayground.setPixelColor(0,0,0,0);
 //A3
 valueA3 = analogRead(A3);
 if(valueA3 <touch){
- CircuitPlayground.setPixelColor(0,0xfcba03);
- CircuitPlayground.setPixelColor(1,0xfcba03);
-  CircuitPlayground.setPixelColor(2,0xfcba03);
- CircuitPlayground.setPixelColor(3,0xfcba03);
-  CircuitPlayground.setPixelColor(4,0xfcba03);
- CircuitPlayground.setPixelColor(5,0xfcba03);
-  CircuitPlayground.setPixelColor(6,0xfcba03);
- CircuitPlayground.setPixelColor(7,0xfcba03);
-  CircuitPlayground.setPixelColor(8,0xfcba03);
- CircuitPlayground.setPixelColor(9,0xfcba03);
+ CircuitPlayground.setPixelColor(0,c);
+ CircuitPlayground.setPixelColor(1,c);
+  CircuitPlayground.setPixelColor(2,c);
+ CircuitPlayground.setPixelColor(3,c);
+  CircuitPlayground.setPixelColor(4,c);
+ CircuitPlayground.setPixelColor(5,c);
+  CircuitPlayground.setPixelColor(6,c);
+ CircuitPlayground.setPixelColor(7,c);
+  CircuitPlayground.setPixelColor(8,c);
+ CircuitPlayground.setPixelColor(9,c);
 
   CircuitPlayground.playTone(247,428.5,false);
   delay(10);
@@ -354,16 +369,16 @@ CircuitPlayground.setPixelColor(0,0,0,0);
 //A4
 valueA4 = analogRead(A4);
 if(valueA4 <touch){
- CircuitPlayground.setPixelColor(0,0xfcba03);
- CircuitPlayground.setPixelColor(1,0xfcba03);
-  CircuitPlayground.setPixelColor(2,0xfcba03);
- CircuitPlayground.setPixelColor(3,0xfcba03);
-  CircuitPlayground.setPixelColor(4,0xfcba03);
- CircuitPlayground.setPixelColor(5,0xfcba03);
-  CircuitPlayground.setPixelColor(6,0xfcba03);
- CircuitPlayground.setPixelColor(7,0xfcba03);
-  CircuitPlayground.setPixelColor(8,0xfcba03);
- CircuitPlayground.setPixelColor(9,0xfcba03);
+ CircuitPlayground.setPixelColor(0,c);
+ CircuitPlayground.setPixelColor(1,c);
+  CircuitPlayground.setPixelColor(2,c);
+ CircuitPlayground.setPixelColor(3,c);
+  CircuitPlayground.setPixelColor(4,c);
+ CircuitPlayground.setPixelColor(5,c);
+  CircuitPlayground.setPixelColor(6,c);
+ CircuitPlayground.setPixelColor(7,c);
+  CircuitPlayground.setPixelColor(8,c);
+ CircuitPlayground.setPixelColor(9,c);
 
   CircuitPlayground.playTone(220,461.5*2,false);
   delay(461.5/2);
@@ -446,16 +461,16 @@ CircuitPlayground.setPixelColor(0,0,0,0);
 //A5
 valueA5 = analogRead(A5);
 if(valueA5 <touch){
- CircuitPlayground.setPixelColor(0,0xfcba03);
- CircuitPlayground.setPixelColor(1,0xfcba03);
-  CircuitPlayground.setPixelColor(2,0xfcba03);
- CircuitPlayground.setPixelColor(3,0xfcba03);
-  CircuitPlayground.setPixelColor(4,0xfcba03);
- CircuitPlayground.setPixelColor(5,0xfcba03);
-  CircuitPlayground.setPixelColor(6,0xfcba03);
- CircuitPlayground.setPixelColor(7,0xfcba03);
-  CircuitPlayground.setPixelColor(8,0xfcba03);
- CircuitPlayground.setPixelColor(9,0xfcba03);
+ CircuitPlayground.setPixelColor(0,c);
+ CircuitPlayground.setPixelColor(1,c);
+  CircuitPlayground.setPixelColor(2,c);
+ CircuitPlayground.setPixelColor(3,c);
+  CircuitPlayground.setPixelColor(4,c);
+ CircuitPlayground.setPixelColor(5,c);
+  CircuitPlayground.setPixelColor(6,c);
+ CircuitPlayground.setPixelColor(7,c);
+  CircuitPlayground.setPixelColor(8,c);
+ CircuitPlayground.setPixelColor(9,c);
 
   CircuitPlayground.playTone(175,500/3,false);
   delay(10);
@@ -567,16 +582,16 @@ CircuitPlayground.setPixelColor(0,0,0,0);
 //A6
 valueA6 = analogRead(A6);
 if(valueA6 <touch){
- CircuitPlayground.setPixelColor(0,0xfcba03);
- CircuitPlayground.setPixelColor(1,0xfcba03);
-  CircuitPlayground.setPixelColor(2,0xfcba03);
- CircuitPlayground.setPixelColor(3,0xfcba03);
-  CircuitPlayground.setPixelColor(4,0xfcba03);
- CircuitPlayground.setPixelColor(5,0xfcba03);
-  CircuitPlayground.setPixelColor(6,0xfcba03);
- CircuitPlayground.setPixelColor(7,0xfcba03);
-  CircuitPlayground.setPixelColor(8,0xfcba03);
- CircuitPlayground.setPixelColor(9,0xfcba03);
+ CircuitPlayground.setPixelColor(0,c);
+ CircuitPlayground.setPixelColor(1,c);
+  CircuitPlayground.setPixelColor(2,c);
+ CircuitPlayground.setPixelColor(3,c);
+  CircuitPlayground.setPixelColor(4,c);
+ CircuitPlayground.setPixelColor(5,c);
+  CircuitPlayground.setPixelColor(6,c);
+ CircuitPlayground.setPixelColor(7,c);
+  CircuitPlayground.setPixelColor(8,c);
+ CircuitPlayground.setPixelColor(9,c);
 
   CircuitPlayground.playTone(659,333/2,false);
   delay(10);
@@ -638,16 +653,16 @@ CircuitPlayground.setPixelColor(0,0,0,0);
 //A7
 valueA7 = analogRead(A7);
 if(valueA7 <touch){
- CircuitPlayground.setPixelColor(0,0xfcba03);
- CircuitPlayground.setPixelColor(1,0xfcba03);
-  CircuitPlayground.setPixelColor(2,0xfcba03);
- CircuitPlayground.setPixelColor(3,0xfcba03);
-  CircuitPlayground.setPixelColor(4,0xfcba03);
- CircuitPlayground.setPixelColor(5,0xfcba03);
-  CircuitPlayground.setPixelColor(6,0xfcba03);
- CircuitPlayground.setPixelColor(7,0xfcba03);
-  CircuitPlayground.setPixelColor(8,0xfcba03);
- CircuitPlayground.setPixelColor(9,0xfcba03);
+  CircuitPlayground.setPixelColor(0,c);
+ CircuitPlayground.setPixelColor(1,c);
+  CircuitPlayground.setPixelColor(2,c);
+ CircuitPlayground.setPixelColor(3,c);
+  CircuitPlayground.setPixelColor(4,c);
+ CircuitPlayground.setPixelColor(5,c);
+  CircuitPlayground.setPixelColor(6,c);
+ CircuitPlayground.setPixelColor(7,c);
+  CircuitPlayground.setPixelColor(8,c);
+ CircuitPlayground.setPixelColor(9,c);
 
   CircuitPlayground.playTone(659,200,false);
   delay(10);
@@ -694,4 +709,6 @@ CircuitPlayground.setPixelColor(0,0,0,0);
   CircuitPlayground.setPixelColor(8,0,0,0);
  CircuitPlayground.setPixelColor(9,0,0,0);
 }
+
+
 }
